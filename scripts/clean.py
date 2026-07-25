cb279bbecb8af6be8a429b6044f501cde1977144
m3m3l0rd101 <huwaeiemployeen101@gmail.com>
2026-07-25 23:00:38 +0000

    windows ver ig

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,14 +1,16 @@
 from PIL import Image ,ImageFilter, ImageEnhance
 import os
-import glob
+
 
 def add_files():
-    files = glob.glob('pics/*',recursive=True)
+    folder = 'pics'
+    files = [os.path.join(folder, f) for f in os.listdir(folder)]
     f = open('all_img.txt', 'w+') 
     f.read()
     for file in files:
         if f != file:
             f.write(file)
+            f.write("\n")
         
 
 
@@ -20,9 +22,11 @@ def clean_up():
     f.read()
     for file in f :
         img = Image.open(str(file))
-        img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+        img = img.filter(ImageFilter.MedianFilter(size=3))
+        img = img.resize((100, 80), Image.LANCZOS) 
+        img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=200, threshold=3))
         img = ImageEnhance.Contrast(img).enhance(1.3)  
-        img = img.resize((100, 80), Image.LANCZOS)
+        
 
 clean_up()
 
